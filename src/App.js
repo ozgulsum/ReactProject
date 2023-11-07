1c78edce3ae95f879eff19285b4f0551bedce79d
Gülsüm Öz <gulsum.oz.44@hotmail.com>
2023-11-07 16:19:08 +0300

    7 Kasım Sınıf Çalışması

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+<html>
+<div className='header'>
+<ul class="menu">
+        <li class="menu-item">
+            <a class="menu-item-link" href="#">
+                <i class="fa fa-home"></i>
+                Anasayfa
+            </a>
+            
+        </li>
+        <li class="menu-item">
+            <a class="menu-item-link" href="#">
+                <i class="fa-regular fa-newspaper"></i>
+                Hakkımızda
+            </a>
+        </li>
+        <li class="menu-item">
+            <a class="menu-item-link galeri" href="#">
+                <i class="fa-sharp fa-solid fa-images"></i>
+                Galeri
+            </a>
+        </li>
+        <li class="menu-item">
+            <a class="menu-item-link" href="#">
+                <i class="fa-solid fa-address-card"></i>
+                İletişim
+            </a>
+        </li>
+    </ul>
+ 
+    <div class="box">
+        Kategoriler
+    </div>
+    <div class="margin1">
+        <p class="padding1">
+            Lorem ipsum dolor sit amet.
+        </p>
+        <p class="padding2">
+            Lorem ipsum dolor sit amet.
+        </p>
+    </div>
+    <div class="margin1">
+        <p class="padding1">
+            Lorem ipsum dolor sit amet.
+        </p>
+        <p class="padding2">
+            Lorem ipsum dolor sit amet.
+        </p>
+    </div>
+    </div>
+    
+
+    </html>
+        ) 
+}
+
+        export default App;
